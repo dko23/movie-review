c500a96735803033ce8a1eca5f04229cdb96225d
Daniel Owusu <danielowusu326@hotmail.co.uk>
2023-06-16 11:29:38 +0000

    Search Feature included

--- src/MovieClass.js
+++ src/MovieClass.js
@@ -1,37 +1,62 @@
 import React, { Component } from 'react';
-
+import  {SearchBar } from './Search';
 
 export class MovieClass extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      movies: []
+      movies: [],
+      filteredMovies: [],
     };
   }
 
   componentDidMount() {
-    fetch("https://api.nytimes.com/svc/movies/v2/reviews/search.json?critics-pick=Y&offset=10&order=by-opening-date&api-key=5SzvsUALdiIAnDsKViXhcAuRvJUrEMp1")
-      .then(res => res.json())
-      .then(data => {
+    fetch(
+      'https://api.nytimes.com/svc/movies/v2/reviews/search.json?critics-pick=Y&offset=10&order=by-opening-date&api-key=5SzvsUALdiIAnDsKViXhcAuRvJUrEMp1'
+    )
+      .then((res) => res.json())
+      .then((data) => {
         console.log(data.results);
-        this.setState({ movies: data.results });
+        this.setState({ movies: data.results, filteredMovies: data.results });
       });
   }
 
+  handleSearch = (searchValue) => {
+    const { movies } = this.state;
+    const filteredMovies = movies.filter((movie) =>
+      movie.display_title.toLowerCase().includes(searchValue.toLowerCase())
+    );
+    this.setState({ filteredMovies });
+  };
+
   render() {
+    const { filteredMovies } = this.state;
+
     return (
-      <div className='container'>
-        <div className='row'>
+      <div className="container">
+        <div className="row">
           <h3>Movies</h3>
-          {this.state.movies.map(movie => (
-            <div className='col-md-4' key={movie.id}>
+          <SearchBar onSearch={this.handleSearch} />
+          {filteredMovies.map((movie) => (
+            <div className="col-md-4" key={movie.id}>
               <div className="card movie-card">
                 <div className="card-body">
                   <h5 className="card-title">Byline: {movie.byline}</h5>
-                  <img src={movie.multimedia.src} className="card-img-top" alt="..." />
-                  <p className="card-text"><strong>Critics Pick: </strong>{movie.critics_pick}</p>
-                  <p className="card-text"><strong>Title:</strong> {movie.display_title}</p>
-                  <p className="card-text"><strong>Headline:</strong> {movie.headline}</p>
+                  <img
+                    src={movie.multimedia.src}
+                    className="card-img-top"
+                    alt="..."
+                  />
+                  <p className="card-text">
+                    <strong>Critics Pick: </strong>
+                    {movie.critics_pick}
+                  </p>
+                  <p className="card-text">
+                    <strong>Title:</strong> {movie.display_title}
+                  </p>
+                  <p className="card-text">
+                    <strong>Headline:</strong> {movie.headline}
+                  </p>
                 </div>
               </div>
             </div>
@@ -41,3 +66,74 @@ export class MovieClass extends Component {
     );
   }
 }
+
+
+
+// ///////import React, { Component } from 'react';
+// import { SearchBar } from './SearchBar';
+
+// export class MovieClass extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       movies: [],
+//       filteredMovies: [],
+//     };
+//   }
+
+//   componentDidMount() {
+//     fetch(
+//       'https://api.nytimes.com/svc/movies/v2/reviews/search.json?critics-pick=Y&offset=10&order=by-opening-date&api-key=5SzvsUALdiIAnDsKViXhcAuRvJUrEMp1'
+//     )
+//       .then((res) => res.json())
+//       .then((data) => {
+//         console.log(data.results);
+//         this.setState({ movies: data.results, filteredMovies: data.results });
+//       });
+//   }
+
+//   handleSearch = (searchValue) => {
+//     const { movies } = this.state;
+//     const filteredMovies = movies.filter((movie) =>
+//       movie.display_title.toLowerCase().includes(searchValue.toLowerCase())
+//     );
+//     this.setState({ filteredMovies });
+//   };
+
+//   render() {
+//     const { filteredMovies } = this.state;
+
+//     return (
+//       <div className="container">
+//         <div className="row">
+//           <h3>Movies</h3>
+//           <SearchBar onSearch={this.handleSearch} />
+//           {filteredMovies.map((movie) => (
+//             <div className="col-md-4" key={movie.id}>
+//               <div className="card movie-card">
+//                 <div className="card-body">
+//                   <h5 className="card-title">Byline: {movie.byline}</h5>
+//                   <img
+//                     src={movie.multimedia.src}
+//                     className="card-img-top"
+//                     alt="..."
+//                   />
+//                   <p className="card-text">
+//                     <strong>Critics Pick: </strong>
+//                     {movie.critics_pick}
+//                   </p>
+//                   <p className="card-text">
+//                     <strong>Title:</strong> {movie.display_title}
+//                   </p>
+//                   <p className="card-text">
+//                     <strong>Headline:</strong> {movie.headline}
+//                   </p>
+//                 </div>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     );
+//   }
+// }
---
+++ src/Search.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+
+export function SearchBar({ onSearch }) {
+  const [searchMovie, setSearchMovie] = useState('');
+
+  const handleInputChange = (event) => {
+    const value = event.target.value;
+    setSearchMovie(value);
+    onSearch(value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        placeholder="Search Movie"
+        value={searchMovie}
+        onChange={handleInputChange}
+      />
+    </div>
+  );
+}
